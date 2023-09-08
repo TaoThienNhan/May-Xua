75f2cc0f7da19263f45a23193f692ff1822a5adf
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-08 13:50:20 +0700

    Phần chào mừng - Trang chủ

---
+++ wp-content/themes/tns-child/template-parts/welcome.php
@@ -0,0 +1,40 @@
+<section class="welcome-wrapper" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/bg-welcome.png');">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-12">
+                <div class="welcome-title">
+                    <h5>Chào mừng đến với Spa May Xưa</h5>
+                    <h2>Nơi Thanh lọc Tâm hồn</h2>
+                    <h4>SINCE 2020</h4>
+                </div>
+                <div class="welcome-text">
+                    <p>
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae <br>
+                        dictab sunt explicabo.
+                    </p>
+                    <p>
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                        magni dolores eos qui ratione voluptatem sequi nesciunt
+                    </p>
+                </div>
+                <div class="welcome-link">
+                    <a href="">Khám phá thêm</a>
+                </div>
+            </div>
+            <div class="welcome-block col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4">
+                <div class="border">
+                        <div class="welcome-block__avatar text-center">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/welcome-avatar.svg" alt="avatar" >
+                        </div>
+                        <p>“Sed ut perspiciatis unde omnis iste <br> natus error sit voluptatem<br> accusantium doloremque ”</p>
+                        <div class="welcome-block__phone">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/welcome_icon.svg" alt="phone"><br>
+                            <a href="">039- 599120</a>
+                        </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
