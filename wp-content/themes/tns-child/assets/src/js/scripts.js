6027216097f273c6e267c8aaf12f51e93b9c38e7
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-11 15:52:32 +0700

    Phần dịch vụ của May Xưa - trang chủ

--- wp-content/themes/tns-child/assets/src/js/scripts.js
+++ wp-content/themes/tns-child/assets/src/js/scripts.js
@@ -1,3 +1,19 @@
 jQuery(function ($) {
-
+    
+$('.service-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+       
+      ]
+  });
 });
---
+++ wp-content/themes/tns-child/template-parts/service.php
@@ -0,0 +1,78 @@
+<section class="service-wrapper" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/bg-welcome.png');">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="service-title text-center">
+                    <h4>Trải nghiệm đặc biệt nhất</h4>
+                    <h2>Dịch vụ của May Xưa</h2>
+                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+                </div>
+            </div>
+        </div>
+        <div class="row service-slider d-flex">
+               
+                    <div class="service-item">
+                        <div class="service-item__img">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
+                        </div>
+                        <div class="service-item__text d-flex ">
+                            <div class="icon "  style=" background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat; z-index: 2;">
+                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                            </div>
+                            <div class="title" >
+                                <h3>Gội đầu dưỡng sinh</h3>
+                                <p>Sed ut perspiciatis unde omnis iste natus</p>
+                            </div>
+                        </div>
+                    </div>
+               
+                    <div class="service-item">
+                        <div class="service-item__img">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-2.png" alt="dịch vụ" >
+                        </div>
+                        <div class="service-item__text d-flex">
+                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon-2.svg'); object-fit: cover; background-repeat: no-repeat;">
+                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                            </div>
+                            <div class="title" >
+                                <h3 style="color: #C89F63;">Body Massage</h3>
+                                <p>Sed ut perspiciatis unde omnis iste natus</p>
+                            </div>
+                        </div>
+                    </div>
+               
+               
+                    <div class="service-item">
+                        <div class="service-item__img">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-1.png" alt="dịch vụ" >
+                        </div>
+                        <div class="service-item__text d-flex">
+                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
+                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                            </div>
+                            <div class="title" >
+                                <h3>Foot Massage</h3>
+                                <p>Sed ut perspiciatis unde omnis iste natus</p>
+                            </div>
+                        </div>
+                    </div>
+               
+                    <div class="service-item">
+                        <div class="service-item__img">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
+                        </div>
+                        <div class="service-item__text d-flex">
+                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
+                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                            </div>
+                            <div class="title" >
+                                <h3>Gội đầu dưỡng sinh</h3>
+                                <p>Sed ut perspiciatis unde omnis iste natus</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+</section>
