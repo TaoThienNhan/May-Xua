5f63d92d383164eaf1aa0162e6c23beb8a1f8f90
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-13 14:43:20 +0700

    Xây dựng trang chi tiết tinh tức

---
+++ wp-content/themes/tns-child/template-parts/details-new.php
@@ -0,0 +1,115 @@
+<div class="header" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/banner-new.png'); object-fit:cover; background-repeat: no-repeat; background-size: cover">
+    <section class="breadcrumb-new">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="breadcrumb__text">
+                        <h2>Tin tức</h2>
+                        <div class="breadcrumb__links text-center">
+                            <a href="./index.html">Trang chủ /</a>
+                            <a href="./index.html">Dịch vụ /</a> 
+                            <span> Gội đầu dưỡng sinh</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<section class="details-new-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+                <div class="title">
+                    <h2>Gội đầu dưỡng sinh</h2>
+                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+               <div class="avatar" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/new-avatar.png'); object-fit:cover; background-repeat: no-repeat; background-size: cover;" >
+               </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+               <div class="text">
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod.</p>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                      Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                    <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                    </p>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+                    <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+               </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+                <div class="related-new text-center">
+                    <h2>Tin tức liên quan</h2>
+                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+                </div>
+            </div>
+        </div>
+        <div class="row new-slider d-flex">
+               <div class="new-item">
+                   <div class="new-item__img">
+                       <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/news_1.png" alt="Tin tức" >
+                       <div class="calendar">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>01.01.2023</span>
+                       </div>
+                   </div>
+                   <div class="new-item__text">
+                        <h3>Eiusmod tempor incid duntut labore.</h3>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praes entium voluptatum deleniti.</p>
+                   </div>
+               </div>
+          
+               <div class="new-item">
+                   <div class="new-item__img">
+                        <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/news_2.png" alt="Tin tức" >
+                        <div class="calendar">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>01.01.2023</span>
+                       </div>
+                   </div>
+                   <div class="new-item__text">
+                         <h3>Eiusmod tempor incid duntut labore.</h3>
+                         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praes entium voluptatum deleniti.</p>
+                   </div>
+               </div>
+               <div class="new-item">
+                   <div class="new-item__img">
+                         <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/news_3.png" alt="Tin tức" >
+                         <div class="calendar">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>01.01.2023</span>
+                       </div>
+                   </div>
+                   <div class="new-item__text">
+                        <h3>Eiusmod tempor incid duntut labore.</h3>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praes entium voluptatum deleniti.</p>
+                   </div>
+               </div>
+          
+               <div class="new-item">
+                   <div class="new-item__img">
+                     <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/news_1.png" alt="Tin tức" >
+                     <div class="calendar">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>01.01.2023</span>
+                       </div>
+                   </div>
+                   <div class="new-item__text">
+                        <h3>Eiusmod tempor incid duntut labore.</h3>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praes entium voluptatum deleniti.</p>
+                   </div>
+               </div>
+           </div>
+    </div>
+</section>
