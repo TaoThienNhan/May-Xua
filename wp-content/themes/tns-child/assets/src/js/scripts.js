0727aa643bb2c2b120732fce581a6653e3beee3d
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-13 14:43:20 +0700

    Xây dựng trang chi tiết tinh tức

--- wp-content/themes/tns-child/assets/src/js/scripts.js
+++ wp-content/themes/tns-child/assets/src/js/scripts.js
@@ -16,4 +16,33 @@ $('.service-slider').slick({
        
       ]
   });
+
+  $('.new-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+    `<button type='button' class='slick-prev pull-left slick-arrow'><i class="fa-solid fa-angle-left"></i></button>`,
+    nextArrow:
+    `<button type='button' class='slick-next pull-right slick-arrow'><i class="fa-solid fa-angle-right"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 1366,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            dots: true,
+          }
+        },
+       
+      ]
+  });
 });
