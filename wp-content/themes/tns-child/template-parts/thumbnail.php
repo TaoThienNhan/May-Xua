e6e5fdcbbb82d7c7e844205eb5bd6cc220f72e39
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-13 19:45:27 +0700

    Phần thư viện hình ảnh gần trên footer - trang chủ

---
+++ wp-content/themes/tns-child/template-parts/thumbnail.php
@@ -0,0 +1,27 @@
+<section class="thumbnail-wrapper">
+    <div class="container-fluid">
+        <div class=" slide-thumbnail">
+              <div class="thumbnail-img">
+                   <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_1.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img" >
+                  <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_2.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img">
+                  <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_3.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img" >
+                  <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_4.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img" >
+                   <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_5.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img">
+                  <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_6.png" alt="thumbnail" >
+              </div>
+              <div class="thumbnail-img">
+                  <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/thumbnail_6.png" alt="thumbnail" >
+              </div>
+        </div>
+    </div>
+</section>
