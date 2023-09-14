0cc956dc76fd65bd75cf05f2108df08b8b7932d2
Tao Thien Nhan <taothiennhan.official@gmail.com>
2023-09-14 13:11:51 +0700

    [Component] upload phần home service

---
+++ wp-content/themes/tns-child/template-parts/home-service.php
@@ -0,0 +1,185 @@
+<style>
+    @font-face {
+        font-family:Be VietNam Pro;
+        src: url("<?=get_stylesheet_directory_uri()?>/assets/src/fonts/Be_Vietnam_Pro");
+    }
+
+    @font-face {
+        font-family:Playfair_Display;
+        src: url("<?=get_stylesheet_directory_uri()?>/assets/src/fonts/Playfair_Display");
+    }
+    @font-face {
+        font-family:Manrone;
+        src: url("<?=get_stylesheet_directory_uri()?>/assets/src/fonts/Manrope");
+    }
+    .carousel-bg {
+        border-radius: 10vh;
+        background-position: center;
+        background: url("<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20318.png");
+        background-size: cover;
+    }
+    .img-right-frist {
+        background: url("<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20319.png");
+        background-size: cover;
+        /*width: 378px;*/
+        height: 733px;
+    }
+    .img-right-last{
+        background: url("<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20320.png");
+        background-size: cover;
+        width: 180px;
+        height: 733px;
+    }
+</style>
+<!--Service-spa start-->
+<div class="content-6 p-5">
+    <div id="carousel-meet-indicator" class="carousel slide mt-5">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carousel-meet-indicator" data-bs-slide-to="0" class=""
+                    aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carousel-meet-indicator" data-bs-slide-to="1" aria-label="Slide 2"
+                    class="active" aria-current="true"></button>
+            <button type="button" data-bs-target="#carousel-meet-indicator" data-bs-slide-to="2" aria-label="Slide 3"
+                    class=""></button>
+        </div>
+
+        <div class="carousel-bg">
+            <div class="container ">
+                <div class="carousel-inner carousel-meet h-auto">
+                    <div class="carousel-item p-5  active">
+                        <div class="slider-1 d-flex justify-content-center align-items-center row">
+                            <div class="d-flex row col-12 col-lg-6 banner-service">
+                                <div class="col-12 col-lg-8 img-right-frist"></div>
+                                <div class="col-12 col-lg-8 img-right-last"></div>
+                                <!--                                    <img src="--><?php //=get_stylesheet_directory_uri()?><!--/assets/src/images/Rectangle%20319.png"  alt="...">-->
+                                <!--                                    <img src="--><?php //=get_stylesheet_directory_uri()?><!--/assets/src/images/Rectangle%20320.png" class=" col-12 col-lg-4" alt="...">-->
+                            </div>
+                            <div class="silder-text col-12 col-lg-6">
+                                <div class="title-header d-flex">
+                                    <span></span>
+                                    <strong>Trải nghiệm dịch vụ chuyên nghiệp</strong>
+                                </div>
+
+                                <h4 class="text-40 text-center text-title my-3">Gọi đầu Dưỡng sinh</h4>
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Vector.png" class="d-inline vector-logo col-12 col-lg-6" alt="...">
+
+                                <br>
+                                <b class="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+                                    vitae dicta sunt explicabo.</b>
+                                <br>
+                                <b>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                                    magni dolores eos qui ratione voluptatem sequi nesciunt.</b>
+                                <div class="d-flex row align-items-center justify-content-center pt-3 btn-list-under">
+                                    <a href=""class="btn btn-under shadow-lg text-20 px-5 my- col-lg-6 col-12">Đặt Lịch</a>
+                                    <div class="d-flex col-lg-6 col-12 align-items-center justify-content-center">
+                                        <div class="col-lg-0 col-3"></div>
+                                        <div class="col-lg-4 col-2 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6579.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-lg-4 col-2 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6578.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-lg-4 col-2 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-lg-0 col-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item p-5  ">
+                        <div class="slider-1 d-flex justify-content-center align-items-center row">
+                            <div class="d-flex row col-12 col-lg-6">
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20319.png" class=" col-12 col-lg-8" alt="...">
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20320.png" class=" col-12 col-lg-4" alt="...">
+                            </div>
+                            <div class="silder-text col-12 col-lg-6">
+                                <div class="title-header d-flex">
+                                    <span></span>
+                                    <strong>Trải nghiệm dịch vụ chuyên nghiệp</strong>
+                                </div>
+
+                                <h4 class="text-40 text-center text-title my-3">Gọi đầu Dưỡng sinh</h4>
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Vector.png" class="d-inline vector-logo col-12 col-lg-6" alt="...">
+
+                                <br>
+                                <b class="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+                                    vitae dicta sunt explicabo.</b>
+                                <br>
+                                <b>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                                    magni dolores eos qui ratione voluptatem sequi nesciunt.</b>
+                                <div class="d-flex row align-items-center justify-content-center pt-3 btn-list-under">
+                                    <a href=""class="btn btn-under shadow-lg text-20 px-5 my- col-lg-6 col-12">Đặt Lịch</a>
+                                    <div class="d-flex col-lg-6 col-12">
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6579.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6578.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group.png"  alt=""></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item p-5  ">
+                        <div class="slider-1 d-flex justify-content-center align-items-center row">
+                            <div class="d-flex row col-12 col-lg-6">
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20319.png" class=" col-12 col-lg-8" alt="...">
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle%20320.png" class=" col-12 col-lg-4" alt="...">
+                            </div>
+                            <div class="silder-text col-12 col-lg-6">
+                                <div class="title-header d-flex">
+                                    <span></span>
+                                    <strong>Trải nghiệm dịch vụ chuyên nghiệp</strong>
+                                </div>
+
+                                <h4 class="text-40 text-center text-title my-3">Gọi đầu Dưỡng sinh</h4>
+                                <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Vector.png" class="d-inline vector-logo col-12 col-lg-6" alt="...">
+
+                                <br>
+                                <b class="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+                                    vitae dicta sunt explicabo.</b>
+                                <br>
+                                <b>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                                    magni dolores eos qui ratione voluptatem sequi nesciunt.</b>
+                                <div class="d-flex row align-items-center justify-content-center pt-3 btn-list-under">
+                                    <a href=""class="btn btn-under shadow-lg text-20 px-5 my- col-lg-6 col-12">Đặt Lịch</a>
+                                    <div class="d-flex col-lg-6 col-12">
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6579.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6578.png"  alt=""></a></div>
+                                        </div>
+                                        <div class="col-4 icon-inner">
+                                            <div class="bg-icon"><a href="" ><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group.png"  alt=""></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-meet-indicator"
+                data-bs-slide="prev">
+            <span class="carousel-control-prev-icon rounded-circle btn-carousel p-3" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next " type="button" data-bs-target="#carousel-meet-indicator"
+                data-bs-slide="next">
+            <span class="carousel-control-next-icon rounded-circle btn-carousel p-3" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<!--Service-spa end-->
--- wp-content/themes/tns-child/template-parts/service.php
+++ wp-content/themes/tns-child/template-parts/service.php
@@ -1,78 +1,69 @@
-<section class="service-wrapper" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/bg-welcome.png');">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,800;1,900&family=Manrope:wght@200;300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,900&display=swap');
+</style>
+
+<div class="services pt-5 bg-dark">
     <div class="container">
-        <div class="row">
-            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                <div class="service-title text-center">
-                    <h4>Trải nghiệm đặc biệt nhất</h4>
-                    <h2>Dịch vụ của May Xưa</h2>
-                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+        <h1 class="text-uppercase text-white text-center play-font my-5 pb-0 text-70">DỊCH VỤ</h1>
+    </div>
+    <div class="premium-servicess bg-white mx-4">
+        <div class="container">
+            <div class="premium mb-5">
+                <div class="title-premium text-18 text-uppercase text-center be-font fw-500" style="letter-spacing: 10px;">PREMIUM</div>
+                <p class="text-premium text-24 text-center be-font fw-500 mt-2 ">Chúng tôi nỗ lực hết mình để cung cấp cho khách hàng những </br> dịch vụ tốt nhất</p>
+            </div>
+
+            <div class="row mt-100 service-container d-flex align-items-center align-items-lg-stretch">
+                <div class="col-md-6 col-left px-0 position-lg-relative">
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/service_1.png" alt="" class="img-fluid w-100 img-service">
+                </div>
+                <div class="col-md-6 mt-lg-5  bg-light p-5  col-right service-box-content position-lg-relative d-flex justify-content-center justify-content-lg-start flex-column align-items-center align-items-lg-start">
+                    <h1 class="text-premium text-45 text-center text-md-start">Massage & Relax</h1>
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/vector.svg" alt="" class="img-fluid my-3" style="max-width: 70px; max-height: 35px;">
+                    <p class="be-font text-18 fw-400 text-lg-start text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis. aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.</p>
+                    <a href="" class=" mt-3 text-15 fw-700 be-font text-uppercase text-decoration-none title-premium">ĐẶT LỊCH CHĂM SÓC</a>
+                    <br>
+                    <a href="" class="btn btn-white btn-right text-15 fw-700 be-font text-uppercase text-decoration-none px-4 py-2 mt-3 mt-lg-0 position-lg-absolute">XEM THÊM</a>
+                </div>
+            </div>
+
+            <div class="row mt-100 service-container d-flex align-items-center align-items-lg-stretch">
+                <div class="col-md-6 order-2 order-lg-1 mt-lg-5  bg-light p-5  col-left service-box-content position-lg-relative d-flex justify-content-center justify-content-lg-start flex-column align-items-center align-items-lg-start" style="z-index: 1;">
+                    <h1 class="text-premium  text-45 text-center text-md-start">Gội đầu dưỡng sinh</h1>
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/vector.svg" alt="" class="img-fluid my-3" style="max-width: 70px; max-height: 35px;">
+                    <p class="be-font text-18 fw-400 text-lg-start text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis. aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.</p>
+                    <a href="" class=" mt-3 text-15 fw-700 be-font text-uppercase text-decoration-none title-premium">ĐẶT LỊCH CHĂM SÓC</a>
+                    <br>
+                    <a href="" class="btn btn-white btn-left text-15 fw-700 be-font text-uppercase text-decoration-none px-4 py-2 mt-3 mt-lg-0 position-lg-absolute">XEM THÊM</a>
+                </div>
+                <div class="col-md-6 order-1 order-lg-2 col-right px-0 position-lg-relative">
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/service_2.png" alt="" class="img-fluid w-100 img-service">
+                </div>
+            </div>
+
+            <div class="row mt-100 service-container d-flex align-items-center align-items-lg-stretch">
+                <div class="col-md-6 col-left px-0 position-lg-relative">
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/service_3.png" alt="" class="img-fluid w-100 img-service">
+                </div>
+                <div class="col-md-6 mt-lg-5  bg-light p-5  col-right service-box-content position-lg-relative d-flex justify-content-center justify-content-lg-start flex-column align-items-center align-items-lg-start">
+                    <h1 class="text-premium text-45 text-center text-md-start">Massage Foot</h1>
+                    <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/vector.svg" alt="" class="img-fluid my-3" style="max-width: 70px; max-height: 35px;">
+                    <p class="be-font text-18 fw-400 text-lg-start text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis. aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.</p>
+                    <a href="" class=" mt-3 text-15 fw-700 be-font text-uppercase text-decoration-none title-premium">ĐẶT LỊCH CHĂM SÓC</a>
+                    <br>
+                    <a href="" class="btn btn-white btn-right text-15 fw-700 be-font text-uppercase text-decoration-none px-4 py-2 mt-3 mt-lg-0 position-lg-absolute">XEM THÊM</a>
                 </div>
             </div>
         </div>
-        <div class="row service-slider d-flex">
-               
-                    <div class="service-item">
-                        <div class="service-item__img">
-                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
-                        </div>
-                        <div class="service-item__text d-flex ">
-                            <div class="icon "  style=" background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat; z-index: 2;">
-                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
-                            </div>
-                            <div class="title" >
-                                <h3>Gội đầu dưỡng sinh</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus</p>
-                            </div>
-                        </div>
-                    </div>
-               
-                    <div class="service-item">
-                        <div class="service-item__img">
-                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-2.png" alt="dịch vụ" >
-                        </div>
-                        <div class="service-item__text d-flex">
-                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon-2.svg'); object-fit: cover; background-repeat: no-repeat;">
-                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
-                            </div>
-                            <div class="title" >
-                                <h3 style="color: #C89F63;">Body Massage</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus</p>
-                            </div>
-                        </div>
-                    </div>
-               
-               
-                    <div class="service-item">
-                        <div class="service-item__img">
-                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-1.png" alt="dịch vụ" >
-                        </div>
-                        <div class="service-item__text d-flex">
-                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
-                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
-                            </div>
-                            <div class="title" >
-                                <h3>Foot Massage</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus</p>
-                            </div>
-                        </div>
-                    </div>
-               
-                    <div class="service-item">
-                        <div class="service-item__img">
-                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
-                        </div>
-                        <div class="service-item__text d-flex">
-                            <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
-                                 <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
-                            </div>
-                            <div class="title" >
-                                <h3>Gội đầu dưỡng sinh</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    </div>
 
+    <div class="container other-service ">
+        <div class="other-service-title d-flex flex-column align-items-center mt-100">
+            <h1 class=" text-white text-center play-font pb-0 text-50">Dịch vụ khác</h1>
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/vector.svg" alt="" class="img-fluid my-3">
+        </div>
+        <div class="other-service-content row mt-100">
+            <!-- có ở trang chủ -->
         </div>
     </div>
-</section>
+</div>
