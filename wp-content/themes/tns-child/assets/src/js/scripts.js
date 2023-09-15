50fe84f19cfd2418472f27deadcc3737e91842bd
haudxpc04339 <108249150+haudxpc04339@users.noreply.github.com>
2023-09-15 08:29:07 +0700

    Xây dựng trang chi tiết dịch vụ

--- wp-content/themes/tns-child/assets/src/js/scripts.js
+++ wp-content/themes/tns-child/assets/src/js/scripts.js
@@ -16,7 +16,34 @@ $('.service-slider').slick({
        
       ]
   });
-
+  $('.service-details__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+    `<button type='button' class='slick-prev pull-left slick-arrow'><i class="fa-solid fa-angle-left"></i></button>`,
+    nextArrow:
+    `<button type='button' class='slick-next pull-right slick-arrow'><i class="fa-solid fa-angle-right"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 1366,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            dots: true,
+          }
+        },
+       
+      ]
+  });
   $('.new-slider').slick({
     infinite: true,
     slidesToShow: 3,
---
+++ wp-content/themes/tns-child/template-parts/details-service.php
@@ -0,0 +1,140 @@
+<div class="header" style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/banner-service.png'); object-fit:cover; background-repeat: no-repeat; background-size: cover">
+    <section class="breadcrumb-new">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="breadcrumb__text">
+                        <h2>Dịch vụ</h2>
+                        <div class="breadcrumb__links text-center">
+                            <a href="./index.html">Trang chủ /</a>
+                            <a href="./index.html">Dịch vụ /</a> 
+                            <span> Gội đầu dưỡng sinh</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<section class="details-service-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+                <div class="title">
+                    <h2>Gội đầu dưỡng sinh</h2>
+                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+                </div>
+            </div>
+        </div>
+        <div class="row service__details__content">
+            <div class="col-xxl-7 col-xl-7 col-lg-7 col-md-12 col-sm-12 px-4">
+                <div class="service__avatar">
+                     <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-avatar2.png" alt="la" >
+                </div>
+                <div class="service__text">
+                    <p>
+                         At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod.
+                    </p>
+                    <p>
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                    </p>
+                    <p>
+                         Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                    </p>
+                    <p>
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                    </p>
+                </div>
+            </div>
+            <div class="col-xxl-5 col-xl-5 col-lg-5 col-md-12 col-sm-12 px-3">
+                <div class="service__avatar">
+                     <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service_avatar.png" alt="la" width="100%">
+                </div>
+                <div class="service__text">
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+                    <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum.</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="book-btn">
+                    <a href=""> Đặt lịch</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="title text-center">
+                    <h3>Các dịch vụ khác</h3>
+                    <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service_icon.svg" alt="la" >
+                </div>
+            </div>
+        </div>
+        <div class="row service-details__slider d-flex">
+               
+               <div class="service-item">
+                   <div class="service-item__img">
+                       <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
+                   </div>
+                   <div class="service-item__text d-flex ">
+                       <div class="icon"  style=" background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat; z-index: 2;">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                       </div>
+                       <div class="service-item__title" >
+                           <h4>Gội đầu dưỡng sinh</h4>
+                           <p>Sed ut perspiciatis unde omnis iste natus</p>
+                       </div>
+                   </div>
+               </div>
+          
+               <div class="service-item">
+                   <div class="service-item__img">
+                       <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-2.png" alt="dịch vụ" >
+                   </div>
+                   <div class="service-item__text d-flex">
+                       <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon-2.svg'); object-fit: cover; background-repeat: no-repeat;">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                       </div>
+                       <div class="service-item__title" >
+                           <h4 style="color: #C89F63;">Body Massage</h4>
+                           <p>Sed ut perspiciatis unde omnis iste natus</p>
+                       </div>
+                   </div>
+               </div>
+          
+          
+               <div class="service-item">
+                   <div class="service-item__img">
+                       <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-1.png" alt="dịch vụ" >
+                   </div>
+                   <div class="service-item__text d-flex">
+                       <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                       </div>
+                       <div class="service-item__title" >
+                           <h4>Foot Massage</h4>
+                           <p>Sed ut perspiciatis unde omnis iste natus</p>
+                       </div>
+                   </div>
+               </div>
+          
+               <div class="service-item">
+                   <div class="service-item__img">
+                       <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/service-3.png" alt="dịch vụ" >
+                   </div>
+                   <div class="service-item__text d-flex">
+                       <div class="icon"  style="background: url('<?= get_stylesheet_directory_uri()?>/assets/src/images/service/bg-icon.svg'); background-repeat: no-repeat;">
+                            <img src="<?= get_stylesheet_directory_uri()?>/assets/src/images/service/icon.svg" alt="la" >
+                       </div>
+                       <div class="service-item__title" >
+                           <h4>Gội đầu dưỡng sinh</h4>
+                           <p>Sed ut perspiciatis unde omnis iste natus</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+    </div>
+</section>
