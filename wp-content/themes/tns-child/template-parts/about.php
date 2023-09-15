03ded71355bc23b389e5e55a11d90caea8336c3a
Hiep-Truong <92008097+Hiep-Truong@users.noreply.github.com>
2023-09-15 11:02:42 +0700

    [Template_parts} _about.php

---
+++ wp-content/themes/tns-child/template-parts/about.php
@@ -0,0 +1,89 @@
+<style>
+    .banner{
+        background: url("<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle309.png");
+        background-size: cover;
+        background-repeat: no-repeat;
+        /*max-height: 595px;*/
+        /*height: 100%;*/
+        height: 600px;
+    }
+
+    .menu-between{
+        place-content: center;
+    }
+
+    .navbar-pagination {
+        height: 25%;
+        flex-shrink: 0;
+        border-radius: 20px;
+        border: 1px solid rgba(255, 255, 255, 0.50);
+        background: rgba(0, 0, 0, 0.30);
+        backdrop-filter: blur(40px);
+        place-content: center;
+    }
+    .btn-booking{
+        background-color: #C89F63;
+        color: #fff;
+        text-wrap: nowrap;
+        font-size: 16px;
+    }
+</style>
+
+
+
+<div class="content-8">
+    <div class="banner container-fluid  d-grid">
+        <div class="row title-in-banner mt-lg-5 mt-md-3 mt-1 d-grid" style="place-content: center;">
+            <strong class="text-white" style="font-size: 70px;font-family: Playfair_Display">Giới thiệu</strong>
+        </div>
+    </div>
+
+    <div class="about-content container-fluid bg-white mt-lg-5 mt-md-3 mt-1">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-7 col-12 mt-lg-5 mt-md-3 mt-1">
+                    <div class="mini-about d-flex m-3">
+                        <span style="
+                        width: 50px;
+                        height: 20px;
+                        border-bottom: 1px solid #805346;">
+
+                        </span>
+                        <strong style="font-family: Be Vietnam Pro; font-size: 15px; color: #805346; font-weight: lighter">Về chúng tôi</strong>
+                    </div>
+
+                    <div class="title-about" style="color: #385469;font-family: Playfair_Display;font-size: 50px;font-weight: bolder">
+                        <strong>Spa May Xưa</strong>
+                        <br>
+                        <strong>Dưỡng Sinh & Thanh Lọc</strong>
+                    </div>
+
+                    <div class="content-about" style="font-family: Be VietNam Pro; font-size: 18px;color: #33393D">
+                        <strong style="font-weight: lighter">
+                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+                            similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+                            Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod.
+                            <br>
+                            Maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.
+                        </strong>
+                    </div>
+
+                    <div class="img-about-left">
+                        <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle365.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-5 col-12 mt-3">
+                    <div class="img-about-right mt-lg-5 mt-md-3 mt-1"><img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Rectangle364.png" alt=""></div>
+                    <div class="content-about-right mt-3" style="font-family: Be VietNam Pro; font-size: 18px;color: #33393D">
+                        <strong style="font-weight: lighter">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum.</strong></div>
+                    <div class="slogan">
+                        <img src="<?=get_stylesheet_directory_uri()?>/assets/src/images/Group6770.png" alt="">
+                        <strong style="font-family: Be VietNam Pro;font-style: italic;color: #385469;font-size: 25px">We make the best for all our customers.</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
